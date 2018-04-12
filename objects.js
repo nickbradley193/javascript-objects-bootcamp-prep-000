8c084670ee64bd2047ac2540e93ccc9fed460671
Nick Bradley <nick.bradley.193@pm.me>
2018-04-12 04:37:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,8 @@ var playlist = {
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
+  console.log(playlist);
   return playlist;
 }
+
+console.log(playlist);
