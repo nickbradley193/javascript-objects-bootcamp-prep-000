4823de67c3a2a25aed1268dd9d817e620b5eaa5a
Nick Bradley <nick.bradley.193@pm.me>
2018-04-12 04:28:24 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = {
+  weezer: "Islands in the sun",
+  kanye: "Golddigger"
+};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, { [artistName]: songTitle});
+}
