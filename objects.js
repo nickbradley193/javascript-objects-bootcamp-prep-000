7e338613feb4bef36cb005b2f9cdaa9fb3d924ff
Nick Bradley <nick.bradley.193@pm.me>
2018-04-12 04:34:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,4 @@
-var playlist = {
-  weezer: "Islands in the sun",
-  kanye: "Golddigger"
-};
+
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle });
